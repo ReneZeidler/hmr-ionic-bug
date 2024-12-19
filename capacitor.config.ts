e2de6f30378226778ecf330f5281ad8c5b19a1e6
René Zeidler <rene.zeidler@gmx.de>
2024-12-19 09:29:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hmr-ionic-bug',
+  webDir: 'www'
+};
+
+export default config;
